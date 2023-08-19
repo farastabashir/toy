0ddc9401815e8e3cbe9d7ebce890b17ed5179eb0
farastabashir <142692979+farastabashir@users.noreply.github.com>
2023-08-20 02:36:22 +0500

    Add files via upload

---
+++ assets/js/comparison-page.js
@@ -0,0 +1,63 @@
+function comparisonone()
+{
+    var productName = document.getElementById("product-1-name").value;
+    var img = document.getElementById("product-1-img");
+    var details = document.getElementById("product-1-details");
+
+    if (productName=="Iphone-X") 
+    {
+        img.src="images/AppleiPhoneX.webp";
+        details.innerHTML="<li>Super Retina HD display</li><li>5.8-inch all-screen OLED display</li><li>HDR display</li><li>2436-by-1125-pixel resolution at 458 ppi</li><li>3D Touch</li>";
+    } 
+    else if (productName=="Iphone-11")
+    {
+        img.src="images/AppleiPhone11.jpg";
+        details.innerHTML="<li>Liquid Retina HD display</li><li>6.1-inch all-screen LCD display with IPS technology</li><li>-</li><li>1792-by-828-pixel resolution at 326 ppi</li><li>Haptic Touch</li>";
+    }
+    else if (productName=="Iphone-12")
+    {
+        img.src="images/AppleiPhone12.webp";
+        details.innerHTML="<li>Super Retina HD display</li><li>5.8-inch all-screen OLED display</li><li>HDR display</li><li>2436-by-1125-pixel resolution at 458 ppi</li><li>3D Touch</li>";
+    }
+    else if (productName=="Iphone-13")
+    {
+        img.src="images/AppleiPhone13.webp";
+        details.innerHTML="<li>Liquid Retina HD display</li><li>6.1-inch all-screen LCD display with IPS technology</li><li>-</li><li>1792-by-828-pixel resolution at 326 ppi</li><li>Haptic Touch</li>";
+    }
+    else
+    {
+        
+    }
+}
+
+function comparisontwo()
+{
+    var productName = document.getElementById("product-2-name").value;
+    var img = document.getElementById("product-2-img");
+    var details = document.getElementById("product-2-details");
+
+    if (productName=="Iphone-X") 
+    {
+        img.src="images/AppleiPhoneX.webp";
+        details.innerHTML="<li>Super Retina HD display</li><li>5.8-inch all-screen OLED display</li><li>HDR display</li><li>2436-by-1125-pixel resolution at 458 ppi</li><li>3D Touch</li>";
+    } 
+    else if (productName=="Iphone-11")
+    {
+        img.src="images/AppleiPhone11.jpg";
+        details.innerHTML="<li>Liquid Retina HD display</li><li>6.1-inch all-screen LCD display with IPS technology</li><li>-</li><li>1792-by-828-pixel resolution at 326 ppi</li><li>Haptic Touch</li>";
+    }
+    else if (productName=="Iphone-12")
+    {
+        img.src="images/AppleiPhone12.webp";
+        details.innerHTML="<li>Super Retina HD display</li><li>5.8-inch all-screen OLED display</li><li>HDR display</li><li>2436-by-1125-pixel resolution at 458 ppi</li><li>3D Touch</li>";
+    }
+    else if (productName=="Iphone-13")
+    {
+        img.src="images/AppleiPhone13.webp";
+        details.innerHTML="<li>Liquid Retina HD display</li><li>6.1-inch all-screen LCD display with IPS technology</li><li>-</li><li>1792-by-828-pixel resolution at 326 ppi</li><li>Haptic Touch</li>";
+    }
+    else
+    {
+        
+    }
+}
